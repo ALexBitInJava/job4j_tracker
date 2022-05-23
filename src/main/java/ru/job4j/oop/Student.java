5a2f4e8e359fa5c6085308bd8e135c8bed565aab
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-05-23 23:33:50 +0300

    Вызов метода объекта

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -9,6 +9,7 @@ public class Student {
     public void music() {
         System.out.println("Tra - tra - tra");
     }
+
     public static void main(String[] args) {
         Student petya = new Student();
         petya.music();
