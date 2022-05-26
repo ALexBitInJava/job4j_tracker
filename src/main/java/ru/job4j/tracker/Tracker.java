0dad5047076791428c625f73a3b81dced8903fda
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-05-27 00:06:28 +0300

    Tracker - хранилище

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Objects;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,28 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < items.length; i++) {
+            Item name = items[i];
+            if (Objects.equals(name.getName(), key)) {
+                rsl[size] = name;
+                size++;
+            }
+        }
+        return rsl;
+    }
+
+    public int findById(int id) {
+        int rsl = 0;
+        for (int i = 0; i < items.length; i++) {
+            Item byId = items[i];
+            if (byId.getId() != id) {
+                return 0;
+        }
+        return id;
     }
 }
