3758eee115c9ce6456c318ecf6c757754989c446
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-05-29 20:12:58 +0300

    Реализация меню за счет шаблона стратегия.

---
+++ src/main/java/ru/job4j/tracker/ExitAction.java
@@ -0,0 +1,14 @@
+package ru.job4j.tracker;
+
+public class ExitAction implements UserAction {
+    @Override
+    public String name() {
+        return "Exit the menu";
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        System.out.println("Thank you. Goodbye!");
+        return false;
+    }
+}
