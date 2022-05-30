f8d72786391993d8f9c970cd24cef5019a7c5acb
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-05-31 00:31:28 +0300

    Пользовательские исключения.

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -1,19 +1,7 @@
 package ru.job4j.ex;
 
 public class FindEl {
-    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
-        int rsl = -1;
-        for (int index = 0; index < value.length; index++) {
-            if (value[index].equals(key)) {
-                rsl = index;
-                break;
-            }
-        }
-        if (rsl == -1) {
-            throw new ElementNotFoundException("Element not found in array");
-        }
-        return rsl;
-    }
+
 
     public static boolean sent(String value, String[] abuses) throws ElementAbuseException {
         /* if contains throw ElementAbuseException */
