1859e45835b2d2c0d9583b192e7f85cc1153d574
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-05-30 21:46:36 +0300

    Пользовательские исключения.

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -3,15 +3,31 @@ package ru.job4j.ex;
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
-        if (key == null) {
-            throw new ElementNotFoundException("Array index not found");
-        }
-        for (int index = 0; index < value.length; index++) {
-            if (value[index].equals(key)) {
-                rsl = index;
+        for (String s : value) {
+            if (!s.equals(key)) {
+                throw new ElementNotFoundException("Array index not found");
             }
         }
         return rsl;
+    }
+
+    public static boolean sent(String value, String[] abuses) throws ElementAbuseException {
+        for (String a : abuses) {
+            if (value.equals(a)) {
+                throw new ElementAbuseException("The key is included in the private keys");
+            }
+        }
+        return true;
+    }
+
+    public static void process(String[] values, String key, String[] abuses) {
+        try {
+            if (indexOf(values, key) != -1) {
+                sent(key, abuses);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
