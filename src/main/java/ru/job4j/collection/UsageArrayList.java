8d163036b680422b58bf1c78885888ccb9b10a2c
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-06-01 23:09:49 +0300

    Коллекции, ArrayList, List, Обобщения

---
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+
+public class UsageArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> collection = new ArrayList<>();
+        collection.add("Petr");
+        collection.add("Stas");
+        collection.add("Elena");
+        for (String value: collection) {
+            System.out.println(value);
+        }
+    }
+}
