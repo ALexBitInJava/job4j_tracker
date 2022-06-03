e721365f884884249c4b1a867afc34a4e50b7769
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-06-03 15:58:46 +0300

    Паспорт и жители

---
+++ src/test/java/ru/job4j/collection/PasspirtOfficeTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PasspirtOfficeTest {
+
+    @Test
+    public void add() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PasspirtOffice office = new PasspirtOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void doubleAdd() {
+        Citizen citizen1 = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen3 = new Citizen("2f44a34", "Alex Golovin");
+        Citizen citizen4 = new Citizen("2f44a34", "Alex Golovin");
+        PasspirtOffice office = new PasspirtOffice();
+        office.add(citizen1);
+        office.add(citizen3);
+        assertFalse(office.add(citizen2));
+        assertFalse(office.add(citizen4));
+    }
+}
